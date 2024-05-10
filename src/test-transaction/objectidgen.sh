7e0768180e8d377bf5a35ec7449c45186005306b
AndersBjerregaard <don-sbn@hotmail.com>
2024-05-10 15:42:34 +0200

    fixed typo in objectid.sh

--- src/test-transaction/objectidgen.sh
+++ src/test-transaction/objectidgen.sh
@@ -1,6 +1,6 @@
 generate_mongo_objectid() {
     length=24
-    objectid=$(tr -dc 'a-z0-9' < /dev/urandom | tr -d '\0')
+    objectid=$(tr -dc 'a-f0-9' < /dev/urandom | tr -d '\0')
     echo "${objectid:0:length}"
 }
 
