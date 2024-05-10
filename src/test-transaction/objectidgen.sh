93edf65ac33f97f38dd7965b485f88f670f38bdb
AndersBjerregaard <don-sbn@hotmail.com>
2024-05-10 15:47:19 +0200

    generate objectid

--- src/test-transaction/objectidgen.sh
+++ src/test-transaction/objectidgen.sh
@@ -1,7 +1,1 @@
-generate_mongo_objectid() {
-    length=24
-    objectid=$(tr -dc 'a-f0-9' < /dev/urandom | tr -d '\0')
-    echo "${objectid:0:length}"
-}
-
-echo $(generate_mongo_objectid)
+echo $(tr -dc 'a-f0-9' < /dev/urandom | head -c 24)
