fc3038f5b2a80543c10726eb5dbfe16e7a393a37
AndersBjerregaard <don-sbn@hotmail.com>
2024-05-12 20:35:43 +0200

    added mandatory flags to driver options, in order to work with ACA

--- src/WebDriverTests/WebDriverXUnit/Helpers/AvailableDriverOptions.cs
+++ src/WebDriverTests/WebDriverXUnit/Helpers/AvailableDriverOptions.cs
@@ -7,9 +7,9 @@ namespace WebDriverXUnit.Helpers;
 
 public static class AvailableDriverOptions {
 
-    // private static readonly DriverOptions[] DRIVER_OPTIONS = [new FirefoxOptions(), new ChromeOptions(), new EdgeOptions()];
+    private static readonly DriverOptions[] DRIVER_OPTIONS = [new FirefoxOptions(), new ChromeOptions(), new EdgeOptions()];
 
-    private static readonly DriverOptions[] DRIVER_OPTIONS = [new EdgeOptions()];
+    public static readonly ChromeOptions CHROME_OPTIONS = new ChromeOptions();
 
     public static DriverOptions[] Get() {
         return DRIVER_OPTIONS;
--- src/WebDriverTests/WebDriverXUnit/Tests/Authentication.cs
+++ src/WebDriverTests/WebDriverXUnit/Tests/Authentication.cs
@@ -1,5 +1,6 @@
 using System.Collections.ObjectModel;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
 using OpenQA.Selenium.Remote;
 using OpenQA.Selenium.Support.UI;
@@ -131,6 +132,11 @@ public class Authentication : IClassFixture<GridUri>
         for (int i = 0; i < Helpers.AvailableDriverOptions.GetAmount(); i++)
         {
             DriverOptions options = driverOptions[i];
+            if (options.GetType().Name == nameof(ChromeOptions)) {
+                var chromeOptions = options as ChromeOptions;
+                // Chrome flags https://github.com/GoogleChrome/chrome-launcher/blob/main/docs/chrome-flags-for-tools.md
+                chromeOptions.AddArguments(["--no-sandbox", "--disable-dev-shm-usage"]);
+            }
             Task task = Task.Run(() => {
                 RemoteWebDriver? driver = null;
                 Uri? uri = _fixture.WebDriverUri;
